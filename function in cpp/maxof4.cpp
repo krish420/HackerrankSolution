a18570e4c42927fbf48cc46aa849b5b8f7f930d5
krish420 <42693382+krish420@users.noreply.github.com>
2020-10-01 12:08:24 +0530

    Update maxof4.cpp
    I use ternary operator here

--- function in cpp/maxof4.cpp
+++ function in cpp/maxof4.cpp
@@ -4,77 +4,22 @@ using namespace std;
 
 int max_of_four(int a, int b, int c, int d)
 {
-    int result;
-    if((a>b)&& (c>d))
-    {
-       if(a>c)
-       {
-           result=a;
-           printf("%d",result);
+        
+    return (((a>b)&&(a>c)&&(a>d))?a:(((b>a)&&(b>c)&&(b>d))?b:(((c>a)&&(c>b)&&(c>d))?c:d)));//here I use the ternary operator to optomize the code
+    /* syntax of ternary operator: 
+        variable = Expression1 ? Expression2 : Expression3
 
-
-       }
-       else
-       {
-           result=c;
-           printf("%d",result);
-
-
-       }
-    }
-    else if ((b>a)&& (c>d))
-    {
-        if(b>c)
+        or 
+        It can be visualized into if-else statement as:
+        if(Expression1)
         {
-            result=b;
-            printf("%d",result);
-
-
+            variable = Expression2;
         }
         else
         {
-            result=c;
-            printf("%d",result);
-
-
+            variable = Expression3;
         }
-
-    }
-    else if ((a>b)&& (d>c))
-    {
-        if(a>d)
-        {
-            result=a;
-            printf("%d",result);
-
-
-        }
-        else
-        {
-            result=d;
-            printf("%d",result);
-
-
-        }
-    }
-    else if ((b>a)&& (d>c))
-    {
-        if(b>d)
-        {
-            result =b;
-            printf("%d",result);
-
-
-        }
-        else
-        {
-            result=d;
-            printf("%d",result);
-
-
-        }
-    }
-    return 0;//if a non-void function is declared then return 0 is must ,without this u will see return type error in hackerrank//
+    */
 }
 int main()
 {
@@ -82,8 +27,8 @@ int main()
     scanf("%d %d %d %d", &p, &q, &r, &s);
     int ans = max_of_four(p,q,r,s);
     //printf("%d", ans);
+    cout << ans;
 
     return 0;
 
 }
-
